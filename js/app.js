571dd57b1de354702be338ef4ccd4ce0448dee9b
roaaalhanbali <roaalhanbali@gamil.com>
2023-05-23 20:34:44 +0000

    Add changes

--- js/app.js
+++ js/app.js
@@ -1,80 +1,57 @@
+// Employee Constructor Function
+function Employee(fullName, department, level) {
+  this.fullName = fullName;
+  this.department = department;
+  this.level = level;
+  this.employeeId = generateEmployeeId();
+  this.salary = this.generateSalary();
+}
 
-// Employee Data
-const employees = [
-  {
-      fullName: "Ahmad Basha",
-      salary: 1800,
-      department: "Administration"
-  },
-  {
-      fullName: "Yara Saddam",
-      salary: 1200,
-      department: "Marketing"
-  },
-  {
-      fullName: "Bahaa Joho",
-      salary: 800,
-      department: "Development"
-  },
-  {
-      fullName: "Rand Mohanad",
-      salary: 950,
-      department: "Finance"
+// Generate a random salary based on the employee level
+Employee.prototype.generateSalary = function () {
+  switch (this.level) {
+    case "Senior":
+      return Math.floor(Math.random() * (2000 - 1500 + 1)) + 1500;
+    case "Mid-Senior":
+      return Math.floor(Math.random() * (1500 - 1000 + 1)) + 1000;
+    case "Junior":
+      return Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
+    default:
+      return 0;
   }
-];
+};
 
 // Generate a unique four-digit employee ID
 function generateEmployeeId() {
   return Math.floor(1000 + Math.random() * 9000);
 }
 
-// Calculate the employee level based on salary range
-function calculateLevel(salary) {
-  if (salary >= 1500 && salary <= 2000) {
-      return "Senior";
-  } else if (salary >= 1000 && salary <= 1500) {
-      return "Mid-Senior";
-  } else if (salary >= 500 && salary <= 1000) {
-      return "Junior";
-  } else {
-      return "Unknown";
-  }
-}
-
 // Render employee information in the table
-function renderEmployees() {
+Employee.prototype.render = function () {
   const employeeTable = document.getElementById("employee-table");
+  const tbody = employeeTable.querySelector("tbody");
 
-  employees.forEach((employee) => {
-      const row = document.createElement("tr");
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>${this.fullName}</td>
+    <td>${this.employeeId}</td>
+    <td>${this.department}</td>
+    <td>${this.level}</td>
+    <td>$${this.salary}</td>
+  `;
 
-      const employeeId = generateEmployeeId();
-      const employeeDepartment = employee.department;
-      const employeeLevel = calculateLevel(employee.salary);
+  tbody.appendChild(row);
+};
 
-      const employeeName = document.createElement("td");
-      employeeName.textContent = employee.fullName;
-      row.appendChild(employeeName);
+// Create employee instances
+const employees = [
+  new Employee("Ahmad Basha", "Administration", "Senior"),
+  new Employee("Yara Saddam", "Marketing", "Junior"),
+  new Employee("Bahaa Joho", "Development", "Junior"),
+  new Employee("Rand Mohanad", "Finance", "Mid-Senior"),
+];
 
-      const employeeIdElem = document.createElement("td");
-      employeeIdElem.textContent = employeeId;
-      row.appendChild(employeeIdElem);
-
-      const employeeDepartmentElem = document.createElement("td");
-      employeeDepartmentElem.textContent = employeeDepartment;
-      row.appendChild(employeeDepartmentElem);
-
-      const employeeLevelElem = document.createElement("td");
-      employeeLevelElem.textContent = employeeLevel;
-      row.appendChild(employeeLevelElem);
-
-      const employeeSalary = document.createElement("td");
-      employeeSalary.textContent = `$${employee.salary}`;
-      row.appendChild(employeeSalary);
-
-      employeeTable.appendChild(row);
-  });
-}
-
-// Call the renderEmployees function to display employee information on the home page
-renderEmployees();
+// Render employees
+employees.forEach((employee) => {
+  employee.render();
+});
